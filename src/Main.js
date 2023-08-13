e584aa297795d3d8a5e4b924afc271183e1c1541
ashmina <raiashmina404@gmail.com>
2023-08-13 15:54:49 +0545

    updateed

---
+++ src/Main.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+   <>
+   <h5>Main</h5>
+   </>
+  )
+}
+
+export default Main;
---
+++ src/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+   <>
+   <h5>Sidebar</h5>
+   </>
+  )
+}
+
+export default Sidebar
